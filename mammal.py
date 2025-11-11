649a952011343ee97acb0838208798fbd8a80ee4
manraj411 <226014190+manraj411@users.noreply.github.com>
2025-11-11 19:19:45 +0530

    Animal class done

---
+++ mammal.py
@@ -0,0 +1,18 @@
+'''
+File: mammal.py
+Description: Mammal subclass with unique behaviors.
+Author: Manraj Singh Randhawa
+ID: 110480393
+Username: manraj411
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
+
+from animal import Animal
+
+class Mammal(Animal):
+    """Represents mammal animals."""
+    def make_sound(self):
+        return f"{self._name} makes a growling sound."
+
+    def feed_milk(self):
+        return f"{self._name} is feeding milk to its young."
