6d7b34a2e5b66028fd22137840f4c540aae04b4e
manraj411 <226014190+manraj411@users.noreply.github.com>
2025-11-11 19:40:45 +0530

    staff file modified

--- staff.py
+++ staff.py
@@ -22,6 +22,10 @@ class Staff(ABC):
 
 class Zookeeper(Staff):
     """Responsible for feeding and cleaning."""
+    def __init__(self, name: str):
+        # Automatically sets role as "Zookeeper"
+        super().__init__(name, "Zookeeper")
+
     def perform_duty(self):
         return f"{self._name} is feeding and cleaning animal enclosures."
 
@@ -32,12 +36,16 @@ class Zookeeper(Staff):
 
 class Veterinarian(Staff):
     """Responsible for health checks and treatments."""
+    def __init__(self, name: str):
+
+        super().__init__(name, "Veterinarian")
+
     def perform_duty(self):
         return f"{self._name} is checking animal health."
 
     def treat_animal(self, animal, notes):
         """Treat the most recent health issue."""
-        if not animal.health_records:
+        if not animal._health_records:
             return f"No health records found for {animal.name}."
-        animal.health_records[-1].add_treatment(notes)
+        animal._health_records[-1].add_treatment(notes)
         return f"{self._name} treated {animal.name} with notes: {notes}."
